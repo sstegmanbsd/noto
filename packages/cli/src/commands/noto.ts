2f0a76893668b6044f7302708bfcdc5322073b43
snelusha <hello@snelusha.dev>
2025-02-24 04:45:50 +0530

    feat: add copy and apply options

--- packages/cli/src/commands/noto.ts
+++ packages/cli/src/commands/noto.ts
@@ -1,18 +1,35 @@
 import * as p from "@clack/prompts";
 import color from "picocolors";
 
-import type { Command } from "@/types";
+import clipboard from "clipboardy";
 
 import { withAuth } from "@/middleware/auth";
 import { withRepository } from "@/middleware/git";
-import { NotoError } from "@/errors";
+
+import { commit } from "@/utils/git";
+
+import type { Command } from "@/types";
+
 import { generateCommitMessage } from "@/ai";
 
 const command: Command = {
   name: "noto",
   description: "generate commit message",
   usage: "noto [options]",
-  options: [],
+  options: [
+    {
+      type: Boolean,
+      flag: "--copy",
+      alias: "-c",
+      description: "copy the generated commit message to clipboard",
+    },
+    {
+      type: Boolean,
+      flag: "--apply",
+      alias: "-a",
+      description: "commit the generated message directly",
+    },
+  ],
   execute: withAuth(
     withRepository(async (options) => {
       const spin = p.spinner();
@@ -23,12 +40,26 @@ const command: Command = {
 
         const message = await generateCommitMessage(diff);
         spin.stop(color.green(message));
+
+        if (options["--copy"]) {
+          clipboard.writeSync(message);
+          p.log.step(color.dim("copied commit message to clipboard"));
+        }
+
+        if (options["--apply"]) {
+          const success = await commit(message);
+          if (success) {
+            p.log.step(color.dim("commit successful"));
+          } else {
+            p.log.error(color.red("failed to commit changes"));
+          }
+        }
       } catch {
         spin.stop(color.red("failed to generate commit message"), 1);
-        console.log();
         process.exit(1);
+      } finally {
+        process.stdout.write("\n");
       }
-      console.log();
     })
   ),
 };

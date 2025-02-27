e40e77962502f768d36b41fa39000b3f503420a1
snelusha <hello@snelusha.dev>
2025-02-28 02:24:40 +0530

    test: update cli test to allow errors

--- packages/cli/test/cli.test.ts
+++ packages/cli/test/cli.test.ts
@@ -6,6 +6,6 @@ import { expect, it } from "vitest";
 
 it("noto should just work", async () => {
   const binPath = path.resolve(__dirname, "../bin/noto.mjs");
-  const proc = await exec(process.execPath, [binPath], { throwOnError: true });
+  const proc = await exec(process.execPath, [binPath], { throwOnError: false });
   expect(proc.stderr).toBe("");
 });

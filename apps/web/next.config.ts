d9285cf2407d682a34d26ac522a849d9f870b46c
snelusha <hello@snelusha.dev>
2025-03-31 23:54:27 +0530

    feat: add redirect for npm package page

--- apps/web/next.config.ts
+++ apps/web/next.config.ts
@@ -8,6 +8,11 @@ const nextConfig: NextConfig = {
         destination: "https://github.com/snelusha/noto",
         permanent: true,
       },
+      {
+        source: "/npm",
+        destination: "https://www.npmjs.com/package/@snelusha/noto",
+        permanent: true,
+      },
     ];
   },
 };

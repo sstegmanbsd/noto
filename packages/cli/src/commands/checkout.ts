efcc4f653ab9a20ef976e83e3769a79f27bbd8d9
snelusha <hello@snelusha.dev>
2025-04-07 05:07:08 +0530

    feat: add checkout command

---
+++ packages/cli/src/commands/checkout.ts
@@ -0,0 +1,75 @@
+import * as p from "@clack/prompts";
+import color from "picocolors";
+
+import dedent from "dedent";
+
+import { withRepository } from "@/middleware/git";
+
+import { checkout, getCurrentBranch, getBranches } from "@/utils/git";
+import { exit } from "@/utils/process";
+
+import type { Command } from "@/types";
+
+const command: Command = {
+  name: "checkout",
+  description: "checkout a branch",
+  usage: "checkout [options]",
+  execute: withRepository(
+    async (options) => {
+      if (!options.isRepo) {
+        p.log.error(
+          dedent`${color.red("no git repository found in cwd.")}
+            ${color.dim(`run ${color.cyan("`git init`")} to initialize a new repository.`)}`
+        );
+        return await exit(1);
+      }
+
+      const branches = await getBranches();
+      if (!branches) {
+        p.log.error("failed to fetch branches");
+        return await exit(1);
+      }
+
+      const currentBranch = await getCurrentBranch();
+
+      const branch = await p.select({
+        message: "select a branch to checkout",
+        options: branches.map((branch) => ({
+          value: branch,
+          label: color.bold(
+            branch === currentBranch ? color.green(branch) : branch
+          ),
+          hint: branch === currentBranch ? "current branch" : undefined,
+        })),
+        initialValue: currentBranch,
+      });
+
+      if (p.isCancel(branch)) {
+        p.log.error("nothing selected!");
+        return await exit(1);
+      }
+
+      if (!branch) {
+        p.log.error("no branch selected");
+        return await exit(1);
+      }
+
+      if (branch === currentBranch) {
+        p.log.error(`${color.red("already on branch")}`);
+        return await exit(1);
+      }
+
+      const result = await checkout(branch!);
+      if (!result) {
+        p.log.error(`failed to checkout ${color.bold(branch)}`);
+        return await exit(1);
+      }
+
+      p.log.success(`checked out ${color.green(branch)}`);
+      await exit(0);
+    },
+    { enabled: false }
+  ),
+};
+
+export default command;

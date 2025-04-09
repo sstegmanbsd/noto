910edde267cafb47b304c9067e6beeb60904f143
snelusha <hello@snelusha.dev>
2025-04-10 02:52:20 +0530

    refactor: update available ai models

--- packages/cli/src/ai/types.ts
+++ packages/cli/src/ai/types.ts
@@ -7,9 +7,7 @@ export const AvailableModelsSchema = z.enum([
   "gemini-1.5-flash-8b-latest",
   "gemini-1.5-pro",
   "gemini-1.5-pro-latest",
-  "gemini-2.0-flash-lite-preview-02-05",
-  "gemini-2.0-flash-exp",
-  "gemini-2.0-pro-exp-02-05",
+  "gemini-2.0-flash-001",
   "gemini-2.5-pro-exp-03-25",
 ]);
 

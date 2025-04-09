79a247f4a7f2d1f66f506a1b8a076629c65662e9
snelusha <hello@snelusha.dev>
2025-04-09 23:45:35 +0530

    feat: add branch command

---
+++ packages/cli/src/commands/branch.ts
@@ -0,0 +1,115 @@
+import * as p from "@clack/prompts";
+import color from "picocolors";
+
+import clipboard from "clipboardy";
+import dedent from "dedent";
+
+import { withRepository } from "@/middleware/git";
+
+import { getBranches, getCurrentBranch } from "@/utils/git";
+import { exit } from "@/utils/process";
+
+import type { Command } from "@/types";
+
+const current: Command = {
+  name: "current",
+  description: "get current branch",
+  usage: "branch current",
+  execute: withRepository(
+    async (options) => {
+      if (!options.isRepo) {
+        p.log.error(
+          dedent`${color.red("no git repository found in cwd.")}
+            ${color.dim(`run ${color.cyan("`git init`")} to initialize a new repository.`)}`
+        );
+        return await exit(1);
+      }
+
+      const branch = await getCurrentBranch();
+
+      if (!branch) {
+        p.log.error("failed to fetch current branch");
+        return await exit(1);
+      }
+
+      p.log.success(`current branch: ${color.bold(branch)}`);
+      
+      await exit(0);
+    },
+    { enabled: false }
+  ),
+};
+
+const command: Command = {
+  name: "branch",
+  description: "list branches",
+  usage: "branch [options]",
+  options: [
+    {
+      type: Boolean,
+      flag: "--copy",
+      alias: "-c",
+      description: "copy the selected branch to clipboard",
+    },
+    {
+      type: Boolean,
+      flag: "--remote",
+      alias: "-r",
+      description: "list branches including remotes",
+    },
+  ],
+  execute: withRepository(
+    async (options) => {
+      if (!options.isRepo) {
+        p.log.error(
+          dedent`${color.red("no git repository found in cwd.")}
+            ${color.dim(`run ${color.cyan("`git init`")} to initialize a new repository.`)}`
+        );
+        return await exit(1);
+      }
+
+      const remote = options["--remote"];
+
+      const branches = await getBranches(remote);
+      const currentBranch = await getCurrentBranch();
+
+      if (!branches) {
+        p.log.error("failed to fetch branches");
+        return await exit(1);
+      }
+
+      const branch = await p.select({
+        message: "select a branch",
+        options: branches.map((branch) => ({
+          value: branch,
+          label: color.bold(
+            branch === currentBranch ? color.green(branch) : branch
+          ),
+          hint: branch === currentBranch ? "current branch" : undefined,
+        })),
+        initialValue: currentBranch,
+      });
+
+      if (p.isCancel(branch)) {
+        p.log.error("nothing selected!");
+        return await exit(1);
+      }
+
+      if (!branch) {
+        p.log.error("no branch selected");
+        return await exit(1);
+      }
+
+      if (options["--copy"]) {
+        clipboard.writeSync(branch);
+        p.log.success(`${color.green("copied to clipboard!")}`);
+      }
+
+      await exit(0);
+    },
+    { enabled: false }
+  ),
+  subCommands: [current],
+};
+
+export default command;
--- packages/cli/src/commands/index.ts
+++ packages/cli/src/commands/index.ts
@@ -1,12 +1,13 @@
 import noto from "@/commands/noto";
 import prev from "@/commands/prev";
+import branch from "@/commands/branch";
 import checkout from "@/commands/checkout";
 import config from "@/commands/config";
 import help from "@/commands/help";
 
 import type { Command } from "@/types";
 
-const commands: Command[] = [noto, prev, checkout, config, help];
+const commands: Command[] = [noto, prev, branch, checkout, config, help];
 
 export const getCommand = (name: string, cmds: Command[] = commands) => {
   return cmds.find((cmd) => cmd.name === name);

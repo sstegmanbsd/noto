9f6dc6c734886dd127f5c57c1906c436ede0d4a6
snelusha <hello@snelusha.dev>
2025-04-14 04:25:18 +0530

    feat: add site config

---
+++ apps/web/config/site.ts
@@ -0,0 +1,14 @@
+export const siteConfig = {
+  title: "@snelusha/noto",
+  description: "Generate clean commit messages in a snap! ✨",
+  creator: "@sneluha",
+  url: "https://noto.snelusha.dev",
+  keywords: ["commit", "message", "generator", "cli", "tool"],
+  authors: [
+    {
+      name: "Sithija Nelusha",
+      url: "https://snelusha.dev",
+    },
+  ],
+  ogImage: "https://noto.snelusha.dev/og.png",
+};

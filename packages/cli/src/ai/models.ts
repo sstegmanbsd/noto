f666cfc66f518df9862536d9189cfdf137ac4f4e
snelusha <hello@snelusha.dev>
2025-05-19 22:19:06 +0530

    fix: update gemini model name

--- packages/cli/src/ai/models.ts
+++ packages/cli/src/ai/models.ts
@@ -23,7 +23,7 @@ export const models: Record<AvailableModels, LanguageModelV1> = {
   "gemini-1.5-pro": google("gemini-1.5-pro"),
   "gemini-1.5-pro-latest": google("gemini-1.5-pro-latest"),
   "gemini-2.0-flash-001": google("gemini-2.0-flash-001"),
-  "gemini-2.0-flash-exp": google("gemini-2.0-flash-exp"),
+  "gemini-2.0-flash": google("gemini-2.0-flash"),
   "gemini-2.0-flash-lite-preview-02-05": google(
     "gemini-2.0-flash-lite-preview-02-05"
   ),
--- packages/cli/src/ai/types.ts
+++ packages/cli/src/ai/types.ts
@@ -8,7 +8,7 @@ export const AvailableModelsSchema = z.enum([
   "gemini-1.5-pro",
   "gemini-1.5-pro-latest",
   "gemini-2.0-flash-001",
-  "gemini-2.0-flash-exp",
+  "gemini-2.0-flash",
   "gemini-2.0-flash-lite-preview-02-05",
   "gemini-2.5-flash-preview-04-17",
   "gemini-2.5-pro-preview-05-06",
